db0aa111ce0363bbdae895a13293c675f98e9961
Suyash-coder-kumar <suyash1008kumar@gmail.com>
2026-04-04 17:22:20 +0530

    new host

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$x&&02xql3yh-6wi))gbus1_y@49d*=n2d_d@!4ey3p0tsg(@r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['portfolio-site-zjek.onrender.com']
 
 
 # Application definition
